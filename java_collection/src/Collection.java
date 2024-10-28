a6f5ee112c77b82fc9fff8af2d697495e75f1704
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2024-10-29 01:36:19 +0530

    move

---
+++ java_collection/src/Collection.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Collection {
+    public static void main(String[] args) {
+        /*// Create an ArrayList of Integer and add elements to it
+        ArrayList<Integer> collection = new ArrayList<>();
+        collection.add(1);
+        collection.add(2);
+        collection.add(3);
+        collection.add(4);
+        // Convert the ArrayList to an Integer array using a generator function
+        Integer[] array = collection.toArray(Integer[]::new);
+        // Convert the Integer array to an int array
+        int[] intArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            intArray[i] = array[i];
+        }*/
+    }
+
+}
---
+++ java_collection/src/Iterator.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Iterator {
+    public static void main(String[] args) {
+        /*// Create a list and add elements to it
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+// Get an iterator and skip the first element
+        java.util.Iterator<String> iterator = list.iterator();
+        iterator.next(); // Skip the first element
+
+// Perform action for each remaining element
+        iterator.forEachRemaining(System.out::println);*/
+
+
+       /* List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            if ("Python".equals(element)) {
+                iterator.remove(); // Remove the element "Python"
+            }
+        }
+
+        // Print the list after removal
+        list.forEach(System.out::println);*/
+
+    }
+}
